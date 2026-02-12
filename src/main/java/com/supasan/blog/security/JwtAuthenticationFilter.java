ad45f02e7813b26439dfc62dbcd5739e616a6b71
sai <supasan4605@gmail.com>
2026-02-12 18:05:01 +0700

    update JwtAuthenticationFilter

--- src/main/java/com/supasan/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/supasan/blog/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,28 +16,36 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final AuthenticationService authenticationService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = extractToken(request);
-        if(token != null) {
-            UserDetails userDetails = authenticationService.validateToken(token);
 
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails,
-                    null,
-                    userDetails.getAuthorities()
-            );
+        try {
+            String token = extractToken(request);
+            if(token != null) {
+                UserDetails userDetails = authenticationService.validateToken(token);
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
 
-            if(userDetails instanceof BlogUserDetails) {
-                request.setAttribute("userId", ((BlogUserDetails) userDetails).getId());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                if(userDetails instanceof BlogUserDetails) {
+                    request.setAttribute("userId", ((BlogUserDetails) userDetails).getId());
+                }
             }
+        } catch (Exception ex) {
+            //Do not throw exceptions, just don't authenticate the user
+            log.warn("Received invalid auth token");
         }
+        filterChain.doFilter(request, response);
     }
 
     private String extractToken(HttpServletRequest request) {
